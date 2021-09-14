0ccfa20175b821afc590c163694633437ecd7742
Akila Ranasinghe <2019is065@stu.ucsc.cmb.ac.lk>
2021-09-15 03:42:51 +0530

    1st commit

---
+++ aboutus.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/headfoot.css">
+<link rel="stylesheet" href="css/formstyle.css">
+<link rel="stylesheet" href="css/card.css"/>
+</head>
+<body style="background-image:url('images/back.jpg');background-size: 100%;background-repeat: no-repeat;">
+<!-----------HEADER START-------------->
+<div class="row header">
+	<img src="images/logS.png">
+	<a href="login.php">LOGIN</a>	
+	<a href="faq.php">FAQ</a>
+	<a href="#" class="active">ABOUT US</a>
+	<a href="index.html">HOME</a>
+</div>
+<!-----------HEADER END-------------->
+<!-----------BODY START-------------->
+<div class="row">
+	<div class="column common" align="center">
+		<img src="images/logL.png" style="width: 200px">
+		<div style="color:white;padding-left:50px;padding-right:50px;">
+			<div class="input-form">
+				<h1>ABOUT US</h1>
+				<p style="width:80%;font-size: 16px;">Improvenest is being built as a platform to interconnect sport related communities to 
+				effectively and efficiently manage individual trainings of sportsmen and sports events. 
+				It facilitates Players, Coaches, Organizers and Sports Federations to connect with each other. 
+				Providing a long term practical solution for the identified problem is the goal of this system. 
+				The measures this system offers are developed with the consultation of the professionals in the field.
+				This project will enhance the current sports competitiveness of athletes by providing necessary 
+				facilities and it will encourage others who have an interest in sports to enter the sports industry.</p>
+				<div class="row">
+					<div class="ite">
+						<div class="card">
+							<img src="images/avatar.png" class="imge">
+							<h3>Member 1</h3>
+						</div>			
+					</div>
+					<div class="ite">
+						<div class="card">
+							<img src="images/avatar.png" class="imge">
+							<h3>Member 2</h3>
+						</div>			
+					</div>
+					<div class="ite">
+						<div class="card">
+							<img src="images/avatar.png" class="imge">
+							<h3>Member 3</h3>
+						</div>			
+					</div>
+					<div class="ite">
+						<div class="card">
+							<img src="images/avatar.png" class="imge">
+							<h3>Member 4</h3>
+						</div>			
+					</div>					
+				</div>
+			</div>
+			<br/>
+		</div>
+		<div class="column backimage"></div>
+	</div>
+</div>
+<br/><br/>
+<!-----------BODY END-------------->
+<!-----------FOOTER START-------------->
+<?php include 'php/commonfooter.php';?>
+<!-----------FOOTER END-------------->
+</body>
+</html>
---
+++ php/commonfooter.php
@@ -0,0 +1,33 @@
+<div class="row footer">
+	<div class="column colthird" align="center">
+		<div class="column colfot1">
+		</div>
+		<div class="column colfot2">
+			<img class="logo" src="images/phone.png" width="50" height="50" alt="phone">
+		</div>
+		<div class="column colfot3">
+			HOTLINE
+			<br><br>
+			0115610123
+		</div>
+	</div>
+	<div class="column colthird" align="center">
+		<br>
+		<h1><a href="contactus.php" style="text-decoration:none;color:black">Contact Us</a></h1>
+	</div>
+	<div class="column colthird" align="right">
+		<br>
+		<div class="column colicon">
+			<a href="#"><img class="logo" src="images/insta.png" width="35" height="35" alt="insta"></a>
+		</div>
+		<div class="column colicon">
+			<a href="#"><img class="logo" src="images/whats.png" width="35" height="35" alt="whats"></a>
+		</div>
+		<div class="column colicon">
+			<a href="#"><img class="logo" src="images/face.png" width="35" height="35" alt="face"></a>
+		</div>
+		<div class="column colicon">
+			<a href="#"><img class="logo" src="images/twitter.png" width="35" height="35" alt="twitter"></a>
+		</div>
+	</div>
+</div>
